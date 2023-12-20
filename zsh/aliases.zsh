2e6c0aa72e8f68918887535e5fd5fca250346d66
Anurag Tilwe <anurag.tilwe6@gmail.com>
2023-12-20 03:34:31 -0500

    Add and modify ls/exa colors

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,15 @@
 # zsh
 alias src='source $ZDOTDIR/.zshrc'
 
-# list
-alias ls='ls -CF --color=auto'
-alias ll='ls -alF --color=auto'
-alias la='ls -A --color=auto'
-alias l='ls -ltrF --color=auto'
+# list (use exa instead of ls)
+# man 5 exa_colors to customize colors and LS_COLORS
+alias ls='exa -s=type' # alias ls='ls -CF --color=auto'
+alias ll='exa -lah --icons -s=type --no-user' # alias ll='ls -alF --color=auto'
+alias la='exa -lah --icons -s=type --no-user' # alias la='ls -A --color=auto'
+alias l='exa -lh --icons -s=type --no-user' # alias l='ls -ltrF --color=auto'
+
+# cat/bat
+alias bat='cat'
 
 # vim
 alias nv='nvim'
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -9,3 +9,6 @@ export EDITOR="nvim"
 export VISUAL="nvim"
 export TERMINAL="alacritty"
 export BROWSER="firefox"
+
+export LS_COLORS='fi=00:di=01;04;36:ex=01;32:ln=35:or=38;5;128:.*rc=33:*.cpp=38;5;12:*.cc=38;5;12:*.c=38;5;12:*.h=38;5;128:*.hpp=38;5;128:*.py=38;5;12:*.txt=38;5;248'
+export EXA_COLORS='da=38;5;242:sn=38;5;242:ng=31:nt=31:sb=38;5;242:ug=31:ut=31:ur=38;5;242:uw=38;5;242:ux=38;5;242:ue=38;5;242:gr=38;5;242:gw=38;5;242:gx=38;5;242:tr=38;5;242:tw=38;5;242:tx=38;5;242'
