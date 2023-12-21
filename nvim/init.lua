dc0db40daa193cb7603f91d601b62c36a24997a4
Anurag Tilwe <anurag.tilwe6@gmail.com>
2023-12-21 15:17:12 -0500

    Plugin manager, theme, lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,17 +3,67 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '	
 
 -------------
+-- Plugins --
+-------------
+-- Lazy Plugin Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath) -- runtime path
+
+-- Configure Plugins
+require("lazy").setup({
+    -- Colorscheme
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        opts = { style = "deep" },
+        config = function(_, opts)
+            require("onedark").setup(opts)
+            vim.cmd.colorscheme "onedark"
+        end,
+    },
+
+    -- Status line
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = "onedark",
+                component_separators = '|',
+                section_separators = '',
+            },
+            sections = {
+                lualine_b = { "filename" },
+                lualine_c = { "branch", "searchcount", "selectioncount" },
+            },
+        },
+    },
+})
+
+
+-------------
 -- Options --
 -------------
+-- Numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Indentation
-vim.o.breakindent = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+vim.o.breakindent = true
 
 -- Searching
 vim.o.incsearch = true
@@ -21,16 +71,15 @@ vim.o.hlsearch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Misc
-vim.o.mouse = 'a'
+-- Window
 vim.o.termguicolors = true
-vim.o.undofile = true
+vim.o.scrolloff = 999
+vim.o.splitright = true
+vim.o.splitbelow = true
 
-vim.cmd("nmap Q <Nop>")
-
-
------------
--- Theme --
------------
-vim.cmd.colorscheme("lunaperche")
-vim.cmd.highlight({ "Normal", "guibg=#121212", "ctermbg=233" })
+-- Misc
+vim.o.mouse = 'a'               -- enable mouse
+vim.o.undofile = true           -- save changes over multiple sessions
+vim.cmd("nmap Q <Nop>")         -- delete Q key mapping (default enters ex mode)
+vim.opt.virtualedit = "block"   -- visual block allows selection over entire screen
+vim.opt.inccommand = "split"    -- shows preview window of search and replace changes
