206b54ea99257b1191b0d8de6ca46fd57c4955c3
Anurag Tilwe <anurag.tilwe6@gmail.com>
2023-12-26 05:18:42 -0500

    use buffers in lualine instead of bufferline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -12,9 +12,26 @@ return {
         },
         sections = {
             lualine_a = { 'mode' },
-            lualine_b = { 'filename' },
-            lualine_c = { 'branch', 'searchcount', 'selectioncount' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_b = { 'branch' },
+            lualine_c = {
+                {
+                    'buffers',
+                    filetype_name = {
+                        TelescopePrompt = 'Telescope',
+                        NvimTree = 'Explorer',
+                        mason = 'Mason',
+                        lazy = 'Lazy',
+                    },
+                    buffers_color = {
+                        -- active = 'TabLineSel',
+                        -- inactive = 'TabLine',
+                    },
+                    symbols = {
+                        alternate_file = '',
+                    },
+                },
+            },
+            lualine_x = { 'searchcount', 'selectioncount', 'filetype' },
             lualine_y = { 'progress' },
             lualine_z = { 'location' },
         },
