0a92ab3303b13aa5e82aa587b61d7b9f15eddbc0
Anurag Tilwe <anurag.tilwe6@gmail.com>
2024-06-02 14:06:05 -0400

    formatters and autotag

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,19 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			local autotag_status_ok, autotag = pcall(require, "nvim-ts-autotag")
+			if not autotag_status_ok then
+				return
+			end
+
+			autotag.setup({})
+		end,
+	},
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,9 +21,9 @@ local on_attach = function(_, bufnr)
     end, '[W]orkspace [L]ist Folders')
 
     -- Format command
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP '})
+    -- vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    --     vim.lsp.buf.format()
+    -- end, { desc = 'Format current buffer with LSP '})
 end
 
 local config = function()
