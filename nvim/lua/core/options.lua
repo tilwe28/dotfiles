2947ae754956fb0ee0dd96eb69269beba50932c2
Anurag Tilwe <anurag.tilwe6@gmail.com>
2024-06-02 23:39:03 -0400

    webdev setup

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -10,7 +10,7 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- sync system clipboard
-opt.clipboard = 'unnamedplus'
+opt.clipboard = "unnamedplus"
 
 -- Indentation
 opt.expandtab = true
@@ -31,60 +31,28 @@ opt.smartcase = true
 opt.termguicolors = true
 opt.splitright = true
 opt.splitbelow = true
-opt.inccommand = 'split'          -- shows preview window of search and replace changes
-vim.wo.signcolumn = 'yes'           -- keep signcolumn on
-opt.showmode = false              -- status line already shows mode
+opt.inccommand = "split" -- shows preview window of search and replace changes
+vim.wo.signcolumn = "yes" -- keep signcolumn on
+opt.showmode = false -- status line already shows mode
 -- opt.showtabline = 2
 
 -- Cursor
 opt.cursorline = true
-opt.mouse = 'a'
+opt.mouse = "a"
 
 -- Decrease update time
 opt.updatetime = 250
 opt.timeoutlen = 300
 
 -- Misc
-opt.undodir = vim.fn.expand('~/.cache/nvim/undodir')
-opt.undofile = true                 -- save undo history
-opt.virtualedit = 'block'           -- visual block allows selection over entire screen
-vim.opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
-vim.opt.iskeyword:append('-')           -- make '-' part of word (opt.t.iskeyword:append('-'))
+opt.undodir = vim.fn.expand("~/.cache/nvim/undodir")
+opt.undofile = true -- save undo history
+opt.virtualedit = "block" -- visual block allows selection over entire screen
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.iskeyword:append("-") -- make '-' part of word (opt.t.iskeyword:append('-'))
 opt.errorbells = false
 opt.swapfile = false
 opt.backup = false
-opt.backspace = 'indent,eol,start'
+opt.backspace = "indent,eol,start"
 opt.autochdir = false
-vim.opt.path:append('**')
-
-----------------------
--- FileType Options --
-----------------------
--- Makefiles need tabs not spaces
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'mk.make',
-    command = 'set noexpandtab'
-})
--- Filetypes with indent size of 2 spaces
-vim.api.nvim_create_augroup('setIndent', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-    group = 'setIndent',
-    pattern = {
-        'scheme',
-        'html',
-        'jsx',
-        'tsx',
-        'javascriptreact',
-        'typescriptreact',
-    },
-    command = 'setlocal tabstop=2 shiftwidth=2 softtabstop=2',
-})
--- Filetypes with indent size of 8 spaces
-vim.cmd('autocmd BufNewFile,BufRead *.as setfiletype asm')
-vim.api.nvim_create_autocmd('FileType', {
-    group = 'setIndent',
-    pattern = {
-        'asm',
-    },
-    command = 'setlocal tabstop=8 shiftwidth=8 softtabstop=8',
-})
+vim.opt.path:append("**")
